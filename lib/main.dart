bfc5e09b4d977fc78676fcf9025e514d3394a6f0
sn0wEasy <yel.bocsmile@gmail.com>
2023-02-10 23:35:59 +0900

    rename: homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:color_tile/screen/playing_page.dart';
 import 'package:color_tile/screen/result_page.dart';
 import 'package:flutter/material.dart';
-import 'package:color_tile/screen/homepage.dart';
+import 'package:color_tile/screen/title_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
