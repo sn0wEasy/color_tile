6833d353a09ae79467ae29880fb322e9257d4d66
sn0wEasy <yel.bocsmile@gmail.com>
2023-02-11 12:21:16 +0900

    add: comment

--- lib/provider/score_provider.dart
+++ lib/provider/score_provider.dart
@@ -7,21 +7,23 @@ import 'dart:math';
 final timeScoreProvider =
     StateProvider<int>((ref) => ref.watch(elapsedTimeProvider) + 1);
 
-// FIXME: スコア計算ロジックを「ブロックごと」から「ブロックの色ごと」に修正する
+// FIXME: スコア算出ロジックが想定通りの挙動になっていないので修正する
+// FIXME: スコア算出ロジックを「ブロックごと」から「ブロックの色ごと」に修正する
 final distanceScoreProvider = Provider<int>((ref) {
   List<BlockModel> blockProfileList = ref.watch(blockModelProvider);
-  double score = 0;
+  double distanceSum = 0;
   blockProfileList.asMap().forEach((_, BlockModel blockProfile) {
-    score += sqrt(pow(blockProfile.finalX - blockProfile.initialX, 2) +
+    distanceSum += sqrt(pow(blockProfile.finalX - blockProfile.initialX, 2) +
         pow(blockProfile.finalY - blockProfile.initialY, 2));
   });
-  final normalizedScore = ((1 / (score + 1)) * pow(10, 6)).toInt();
-  return normalizedScore;
+  final score = ((1 / (distanceSum + 1)) * pow(10, 6)).toInt();
+  return score;
 });
 
+// FIXME: スコア算出ロジックを改良する
 final totalScoreProvider = Provider<int>((ref) {
   int distanceScore = ref.watch(distanceScoreProvider);
   int timeScore = ref.watch(timeScoreProvider);
-  int totalScore = distanceScore ~/ timeScore;
+  int totalScore = distanceScore ~/ sqrt(timeScore);
   return totalScore;
 });
